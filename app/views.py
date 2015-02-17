36f2f0c46f58081c660f8aa26402f817ca4f70c4
theopavlakou <theo.pavlakou10@imperial.ac.uk>
2015-02-17 12:58:04 +0000

    Now has a useless login page (but more infrastructure)

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
-
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -35,6 +35,18 @@ def index():
                            posts=posts,
                            active_home=True,
                            active_about_us=False)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
 
 @app.route('/about_us')
 def about_us():
