9af26286fe9ec0cca900494b4248160496a77012
theopavlakou <theo.pavlakou10@imperial.ac.uk>
2015-02-17 17:04:40 +0000

    Now we have a slider

--- app/server.py
+++ app/server.py
@@ -7,4 +7,4 @@ from app import app
 @app.route("/get_coords", methods=['POST'])
 def get_map_coords():
 
-    return jsonify(request.view_args)
+    return jsonify({'message': 'hi'})
--- app/views.py
+++ app/views.py
@@ -32,7 +32,6 @@ def index():
     return render_template("index.html",
                            title='Home',
                            user=user,
-                           posts=posts,
                            active_home=True,
                            active_about_us=False)
 
