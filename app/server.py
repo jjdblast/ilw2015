c1c035620c10535020dc97d8a07a6c1fb1b408f1
theopavlakou <theo.pavlakou10@imperial.ac.uk>
2015-02-18 10:47:08 +0000

    Going to try and switch maps

--- app/server.py
+++ app/server.py
@@ -8,4 +8,6 @@ from app import app
 def get_map_coords():
     return str(request.form)
     # return jsonify({'message': request.data})
+    #TODO here we must get the Tweets relevant, turn them
+    #into JSON format.
     return jsonify({'message': request.data})
