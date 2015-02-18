526dbd1bf703d110c7fffa9dcb41885e768d85a7
theopavlakou <theo.pavlakou10@imperial.ac.uk>
2015-02-18 16:38:24 +0000

    Now I can get data from the JSON

--- app/server.py
+++ app/server.py
@@ -1,6 +1,7 @@
 __author__ = 'theopavlakou'
 
 from flask import jsonify, request, url_for
+import json
 
 from app import app
 
@@ -10,9 +11,12 @@ def get_map_coords():
     print(request.form["data_value"])
     points = []
     points.append({"location":(55.951663, -3.206273), "weight": 10})
-    points.append({"location":(52.951663, -1.206273), "weight": 10})
-    points.append({"location":(51.951663, -2.206273), "weight": 10})
+    points.append({"location":(55.954708, -3.302416), "weight": 12})
+    points.append({"location":(55.97194, -3.224334), "weight": 2})
+    points.append({"location":(55.959472, -3.249847), "weight": 4})
+    j_points = json.dumps({"points":points})
+    print(j_points)
     print({"points": points})
-    return jsonify({"points": points})
+    return j_points
 
 
